a2bd6bbbeb54e82a1ef4bb79c581477765a594eb
donroad <601189570@qq.com>
2018-02-27 20:00:09 +0800

    oneFieldKeyWordSearch

--- lawDoc/tests.py
+++ lawDoc/tests.py
@@ -1,3 +1,6 @@
 from django.test import TestCase
 
 # Create your tests here.
+from lawDoc.views import indexSearch,searchByStrcut
+
+indexSearch()
--- lawDoc/views.py
+++ lawDoc/views.py
@@ -3,7 +3,7 @@ import json
 from django.shortcuts import render
 from django.http import HttpResponse
 from elasticsearch import Elasticsearch
-from lawDoc.Variable import legalDocuments, allSearchField
+from lawDoc.Variable import legalDocuments, allSearchField, allSearchFieldDict
 
 
 # Create your views here.
@@ -18,11 +18,25 @@ def index(request):
 #首页的搜索，java版本对应路径为“indexsearch”
 def indexSearch(request):
     print("1111111111111111")
-    keyWord=request.POST.get('keyword')
-
     searchStruct=SearchStruct()
-    searchStruct.allFieldKeyWord=keyWord.split(" ")
-    print(searchStruct.allFieldKeyWord)
+    keyWord=request.POST.get('keyword')
+    #keyWord = "杀人 放火@本院认为"
+    #构建搜索结构
+    if "@" in keyWord:
+        if "!" in keyWord:
+            pass
+        else:
+            searchStruct.oneFieldKeyWord = {
+                "field":keyWord.split("@")[1],
+                "keyword":keyWord.split("@")[0].split(" ")
+            }
+    else:
+        if "!" in keyWord:
+            pass
+        else:
+            searchStruct.allFieldKeyWord=keyWord.split(" ")
+            print(searchStruct.allFieldKeyWord)
+
     legalDocuments.clear()
     searchByStrcut(searchStruct)
 
@@ -55,26 +69,34 @@ def getRecommondDetail(request):
 def searchByStrcut(searchStruct):
     #连接es
     es = Elasticsearch()
-    #取出searchstruct中的allFieldKeyWord
-    allFieldKeyWord=searchStruct.allFieldKeyWord
-    allFieldKeyWordQuery=[]
-    allFieldKeyWordMiniQuery=[]
-    #全领域搜索的解决思路是对每个域进行搜索，之间用should连接
-    for i in allFieldKeyWord:
-        for j in allSearchField:
-            allFieldKeyWordMiniQuery.append({"match_phrase":{j:i}})
-        allFieldKeyWordQuery.append({"bool":{"should":allFieldKeyWordMiniQuery}})
+    # #取出searchstruct中的allFieldKeyWord
+    # allFieldKeyWord=searchStruct.allFieldKeyWord
+    # allFieldKeyWordQuery=[]
+    # allFieldKeyWordMiniQuery=[]
+    # #全领域搜索的解决思路是对每个域进行搜索，之间用should连接
+    # for i in allFieldKeyWord:
+    #     for j in allSearchField:
+    #         allFieldKeyWordMiniQuery.append({"match_phrase":{j:i}})
+    #     allFieldKeyWordQuery.append({"bool":{"should":allFieldKeyWordMiniQuery}})
 
-    query = {
-    "query": {
-        "bool": {
-            "must": allFieldKeyWordQuery
+    #单领域搜索
+    if len(searchStruct.oneFieldKeyWord) != 0:
+        oneFieldKeyWord = searchStruct.oneFieldKeyWord["keyword"]
+        oneFieldKeyWordQuery = []
+        field = allSearchFieldDict.get(searchStruct.oneFieldKeyWord["field"])
+        for i in oneFieldKeyWord:
+            oneFieldKeyWordQuery.append({"match_phrase":{field:i}})
+        query = {
+        "query": {
+            "bool": {
+                "must": oneFieldKeyWordQuery
+            }
         }
     }
-}
     print(json.dumps(query))
     results = es.search(index='legal_index', doc_type='lagelDocument', body=json.dumps(query))['hits']['hits']
 
     for result in results:
         lines=result['_source']['byrw'].split("\n")
         print(lines)
+
