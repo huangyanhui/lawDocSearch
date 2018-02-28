d7402866495666cf34092742e720802123e58bae
huangyanhui <1321811187@qq.com>
2018-02-28 21:52:41 +0800

    Update Variable.py

--- lawDoc/Variable.py
+++ lawDoc/Variable.py
@@ -1,7 +1,4 @@
 #储存查询结果后的文书
 legalDocuments=[]
 allSearchField=["dsrxx","ah","spry","ysfycm","ysqqqk","byrw","spjg","ysdbqk","esqqqk","ysfyrw","ajms","xgft","sprq","sljg","bycm","sjy","bt"]
-
-
 allSearchFieldList={"当事人信息":"dsrxx","案号":"ah","审判日期":"spry","一审法院查明":"ysfycm","一审请求情况":"ysqqqk","本院认为":"byrw","审理经过":"spjg","一审答辩情况":"ysdbqk","二审请求情况":"esqqqk","一审法院认为":"ysfyrw","案件描述":"ajms","相关法条":"xgft","审判日期":"sprq","审理经过":"sljg","本院查明":"bycm","书记员":"sjy","标题":"bt"}
-
