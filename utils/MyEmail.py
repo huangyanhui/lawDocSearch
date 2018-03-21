956f4c0f18b147ff89ebfd44d5107d494716270f
Jeafi <651699394@qq.com>
2018-03-21 15:19:27 +0800

    Update MyEmail.py

--- utils/MyEmail.py
+++ utils/MyEmail.py
@@ -13,7 +13,7 @@ def generate_random_str(random_length=4):
 
 def send_your_email(email, send_type='foget_password'):
     code = generate_random_str(4)
-    email_title = '换宴会'
+    email_title = '换密码'
     email_body = '请点击下面的链接修改你的密码: http://127.0.0.1:8000/active/{0}'.format(code)
     send_status = send_mail(email_title, email_body, EMAIL_FROM, [email])
     return send_status
